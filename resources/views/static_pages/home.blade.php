1c6fe7019e72ab8c524028d579ffa496c26e4b1b
richard1230 <yongjinhu1230@gmail.com>
2023-01-26 22:38:38 +0800

    模板切分

--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,9 +1,6 @@
-<!DOCTYPE html>
-<html>
-<head>
-  <title>Weibo App</title>
-</head>
-<body>
-  <h1>主页</h1>
-</body>
-</html>
+@extends('layouts.default')
+@section('content')
+<h1>主页</h1>
+@stop
+
+
